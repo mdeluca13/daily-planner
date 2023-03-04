94ed06a645a9ef609f77128577b045ed2e33a821
mdeluca13 <123423832+mdeluca13@users.noreply.github.com>
2023-03-04 09:31:18 -0500

    Added html for each hour, js to display current day and loop through time to colorcode hour blocks

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,32 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+//Setting Variables
+var timeBlock = $('.time-block');
+var currentDay = $('#current-day');
+var currentHour = dayjs().hour();
+var date = dayjs().format('dddd, MMMM D, YYYY');
+var saveButton = $('.saveBtn');
+
+// Run after window loads fully
+$(window).on('load', function() {
+
+  //Displaying date at top of Planner
+  currentDay.text(date);
+
+  //Looping to check id with the current hour and setting class to change color
+  for (var i = 0; i < timeBlock.length; i++){
+    var timeBlockIndex = $(timeBlock)[i];
+    var hourCheck = timeBlockIndex.id;
+    if (hourCheck == currentHour) {
+      $(timeBlockIndex).addClass('present');
+    }
+    else if (hourCheck > currentHour){
+      $(timeBlockIndex).addClass('future');
+    }
+    else {
+      $(timeBlockIndex).addClass('past');
+    }
+  }
+});
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
